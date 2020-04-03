3ff304be4f4c09462ca449f9b2c070536a38f134
i.const.yank <i.const.yank@gmail.com>
2020-04-03 18:26:42 +0300

    update

--- src/js/full-page-scroll.js
+++ src/js/full-page-scroll.js
@@ -147,7 +147,6 @@
 					_self.changeCurrentPosition(_self.defaults.currentPosition);
 					if (_self.defaults.currentPosition === 6) this_deltaY = '';
 				} else if (event.deltaY < 0 || event.keyCode == 38) {
-
 					_self.defaults.currentPosition --;
 					_self.changeCurrentPosition(_self.defaults.currentPosition);
 					if (_self.defaults.currentPosition === 0) this_deltaY = '';
@@ -155,6 +154,9 @@
 				_self.removeEvents();
 			}
 			this_deltaY = event.target.closest('section').id;
+			setTimeout(function(){
+				this_deltaY = ''
+			}, 5000);
 		};
 
 		this.touchStart = function (event) {
