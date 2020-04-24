6f99c5c92f20d9a20069cd951e2834f098fc0640
i.const.yank <i.const.yank@gmail.com>
2020-04-24 12:06:01 +0300

    update

--- src/ajax/mail.php
+++ src/ajax/mail.php
@@ -13,7 +13,9 @@ $subject = 'Griffith Ventures';
 $headers.= "X-Mailer: PHP/" . phpversion()."\r\n";
 $headers.= "MIME-Version: 1.0" . "\r\n";
 $headers.= "Content-type: text/plain; charset=utf-8\r\n";
+
 $to = "i.const.yank@gmail.com";
+
 $message .= "Name: $name\n";
 $message .= "E-mail: $email\n";
 $message .= "Subject: $sub\n";
